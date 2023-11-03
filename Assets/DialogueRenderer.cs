5698a9eba9c22135f81f0a5e60a0674a0457e7c7
Megan <megan@theparkes.me.uk>
2023-11-03 14:10:36 +0000

    add doos

--- Assets/DialogueRenderer.cs
+++ Assets/DialogueRenderer.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DialogueRenderer : MonoBehaviour
 {
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI Text;
+    public GameObject canvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShowDialogue(List<string> text, string speaker)
+    {
+        Name.text = speaker;
+        Text.text = text[0];
+        canvas.SetActive(true);
+
     }
 }
