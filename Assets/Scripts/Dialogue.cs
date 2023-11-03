21447e0a8405f3984a8484e7fa8a967e97319b12
Megan <megan@theparkes.me.uk>
2023-11-03 15:34:56 +0000

    support dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class Dialogue : MonoBehaviour
+{
+    public List<string> texts;
+    public string CharacterName;
+    private bool interactable = false;
+
+
+    private DialogueRenderer dialogueRenderer;
+
+    void Awake()
+    {
+        dialogueRenderer = FindObjectOfType<DialogueRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        interactable = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        interactable = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && interactable)
+        {
+            if (dialogueRenderer.IsOpen())
+            {
+                dialogueRenderer.closeDialogue();
+            }
+            else
+            {
+                dialogueRenderer.ShowDialogue(texts.PickRandom(), CharacterName);
+            }
+        }
+
+    }
+}
