ee791f5b670903595b9319ca1b3048047d1b878f
adamskipadamski <adam.t.wright@outlook.com>
2023-11-03 10:12:09 +0000

    refactor CharacterController

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,15 +7,22 @@ public class CharacterController : MonoBehaviour
     public LayerMask LayerMask;
 
     private float HorizontalMovement;
-    private float HorizontalDelta;
-    private Vector3 PositionOverride = Vector2.zero;
-
-    private float horizontalAlter;
-    private float verticalAlter;
     private bool Jumping;
-    private float verticalSpeed;
     private bool IsGrounded;
     private Vector2 Velocity;
+    private RaycastHit2D[] RaycastResults = new RaycastHit2D[10];
+    private ContactFilter2D ContactFilter = new ContactFilter2D();
+    private Vector3 PositionOverride = Vector2.zero;
+
+    public void ForcePosition(Vector3 position)
+    {
+        PositionOverride = position;
+    }
+
+    void Awake()
+    {
+        ContactFilter.layerMask = LayerMask;
+    }
 
     void Update()
     {
@@ -24,6 +31,13 @@ public class CharacterController : MonoBehaviour
     }
 
     void FixedUpdate()
+    {
+        UpdateVelocity();
+        UpdatePosition();
+        ResetVelocity();
+    }
+
+    private void UpdateVelocity()
     {
         Velocity.x = HorizontalMovement * HorizontalSpeed;
 
@@ -40,75 +54,35 @@ public class CharacterController : MonoBehaviour
         }
 
         Velocity += Physics2D.gravity * Time.fixedDeltaTime;
-
-        var displacement = Velocity * Time.fixedDeltaTime;
-        RaycastHit2D[] RaycastResults = new RaycastHit2D[15];
-        var contactFilter = new ContactFilter2D();
-        contactFilter.layerMask = LayerMask;
-        Rb2d.SafeMove(Vector2.right * displacement.x, displacement.x, RaycastResults, contactFilter);
-        int RaycastResultCount = Rb2d.SafeMove(Vector2.up * displacement.y, displacement.y, RaycastResults, contactFilter);
-
-        for ( int i = 0; i < RaycastResultCount; i++ )
-        {
-            if (Mathf.Approximately(RaycastResults[i].normal.y, 1))
-            {
-                IsGrounded = true;
-                Velocity.y = 0;
-            }
-        }
-
-        HorizontalDelta = 0;
-        horizontalAlter = 0;
     }
 
-    public void ForcePosition(Vector3 position)
+    private void UpdatePosition()
     {
-        PositionOverride = position;
-    }
-
-    private Vector2 GetDisplacement()
-    {
-        if (PositionOverride == Vector3.zero)
+        if (PositionOverride != Vector3.zero)
         {
-            return new Vector2(HorizontalDelta * Time.fixedDeltaTime, 0);
+            Rb2d.position = PositionOverride;
+            PositionOverride = Vector3.zero;
         }
         else
         {
-            var displacement = PositionOverride;
+            var displacement = Velocity * Time.fixedDeltaTime;
+            
+            Rb2d.SafeMove(Vector2.right * displacement.x, displacement.x, RaycastResults, ContactFilter);
+            int RaycastResultCount = Rb2d.SafeMove(Vector2.up * displacement.y, displacement.y, RaycastResults, ContactFilter);
 
-            PositionOverride = Vector2.zero;
-
-            return displacement;
+            for (int i = 0; i < RaycastResultCount; i++)
+            {
+                if (Mathf.Approximately(RaycastResults[i].normal.y, 1))
+                {
+                    IsGrounded = true;
+                    Velocity.y = 0;
+                }
+            }
         }
     }
 
-    private void GetHorizontalCollisions()
+    private void ResetVelocity()
     {
-        var collider = GetComponent<BoxCollider2D>();
-        var size = collider.size;
-
-        var hit = Physics2D.Raycast(transform.position, new Vector2(HorizontalMovement, 0), size.x / 2 + 0.5f, LayerMask);
-
-        if (hit.collider != null)
-        {
-            var a = (size.x / 2 + 0.5f) - hit.distance;
-            horizontalAlter = HorizontalMovement == -1 ? a : -a;
-        }
-    }
-
-    private void GetVerticalCollisions(float verticalTravel)
-    {
-        var collider = GetComponent<BoxCollider2D>();
-        var size = collider.size;
-
-        var hit = Physics2D.Raycast(transform.position, new Vector2(0, Mathf.Sign(verticalTravel)), size.y / 2 + Mathf.Abs(verticalTravel) + 0.5f, LayerMask);
-
-        if (hit.collider != null)
-        {
-            var a = (size.y / 2 + 0.5f) - hit.distance;
-            verticalAlter = a;
-            verticalSpeed = 0;
-            IsGrounded = true;
-        }
+        Velocity.x = 0;
     }
 }
