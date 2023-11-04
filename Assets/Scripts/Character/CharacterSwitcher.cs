478a6c551bcc4723c6859baa6240a73cca3347fc
adamskipadamski <adam.t.wright@outlook.com>
2023-11-04 10:18:31 +0000

    reset character physics on switch

--- Assets/Scripts/Character/CharacterSwitcher.cs
+++ Assets/Scripts/Character/CharacterSwitcher.cs
@@ -22,13 +22,15 @@ public class CharacterSwitcher : MonoBehaviour
 
         if (FirstActiveCharacter == Giant)
         {
-            GnomeCharacterController.enabled = false;
+            GnomeCharacterController.Freeze();
+            GiantCharacterController.Thaw();
             GnomeImage.color = Color.white.WithAlpha(0.5f);
             GiantImage.color = Color.white.WithAlpha(1f);
         }
         else
         {
-            GiantCharacterController.enabled = false;
+            GnomeCharacterController.Thaw();
+            GiantCharacterController.Freeze();
             GnomeImage.color = Color.white.WithAlpha(1f);
             GiantImage.color = Color.white.WithAlpha(0.5f);
         }
@@ -38,24 +40,25 @@ public class CharacterSwitcher : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
+            Debug.Log("Hello");
             SwitchCharacter();
         }
     }
 
     public void SwitchCharacter()
     {
-        if (GiantCharacterController.enabled)
+        if (GiantCharacterController.IsFrozen())
         {
-            GnomeCharacterController.enabled = true;
-            GiantCharacterController.enabled = false;
+            GnomeCharacterController.Thaw();
+            GiantCharacterController.Freeze();
             CinemachineVirtualCamera.Follow = Gnome.transform;
             GnomeImage.color = Color.white.WithAlpha(1f);
             GiantImage.color = Color.white.WithAlpha(0.5f);
         }
         else
         {
-            GnomeCharacterController.enabled = false;
-            GiantCharacterController.enabled = true;
+            GnomeCharacterController.Freeze();
+            GiantCharacterController.Thaw();
             CinemachineVirtualCamera.Follow = Giant.transform;
             GnomeImage.color = Color.white.WithAlpha(0.5f);
             GiantImage.color = Color.white.WithAlpha(1f);
