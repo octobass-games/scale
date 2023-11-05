494d983bbd5167ac41a76e4d11bd90defb6d7037
adamskipadamski <adam.t.wright@outlook.com>
2023-11-05 17:16:43 +0000

    add PadlockedGate component

---
+++ Assets/Scripts/Gate/PadlockedGate.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PadlockedGate : MonoBehaviour
+{
+    public GameObject PadlockKey;
+    public UnityEvent OnOpen;
+
+    private Inventory InventoryInProximity;
+
+    void Update()
+    {
+        if (InventoryInProximity != null && Input.GetKeyDown(KeyCode.E))
+        {
+            var key = InventoryInProximity.RemoveItem(PadlockKey);
+
+            if (key != null)
+            {
+                Destroy(key.gameObject);
+                OnOpen.Invoke();
+            }
+        }   
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        InventoryInProximity = collision.GetComponent<Inventory>();
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        InventoryInProximity = null;
+    }
+}
