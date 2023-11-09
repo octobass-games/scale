937b0204f92008c829c936f6601ae93308c820f3
Megan <megan@theparkes.me.uk>
2023-11-09 15:43:52 +0000

    display icons

---
+++ Assets/Scripts/DisplayIconOnEnter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class DisplayIconOnEnter : MonoBehaviour
+{
+    public SpriteRenderer Icon;
+    public SpriteRenderer InteractingSprite;
+    public Material Lit;
+    public Material Unlit;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (TagComparer.IsPlayer(collision.tag))
+        {
+            if (Icon != null)
+            {
+                Icon.gameObject.SetActive(true);
+            }
+
+            if (InteractingSprite != null)
+            {
+                InteractingSprite.material = Unlit;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (TagComparer.IsPlayer(collision.tag))
+        {
+            if (Icon != null)
+            {
+                Icon.gameObject.SetActive(false);
+            }
+
+            if (InteractingSprite != null)
+            {
+                InteractingSprite.material = Lit;
+            }
+        }
+    }
+}
