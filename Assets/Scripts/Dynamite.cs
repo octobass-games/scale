8218efb0f47d3ee47e16751413a317339d2f1ce2
adamskipadamski <adam.t.wright@outlook.com>
2023-11-26 15:18:16 +0000

    improve bomb in Level4-3

--- Assets/Scripts/Dynamite.cs
+++ Assets/Scripts/Dynamite.cs
@@ -7,11 +7,6 @@ public class Dynamite : MonoBehaviour
     private float Timer;
     private bool IsTimerPaused = true;
 
-    void Awake()
-    {
-        
-    }
-
     public void PauseTimer()
     {
         IsTimerPaused = true;
@@ -20,6 +15,18 @@ public class Dynamite : MonoBehaviour
     public void UnpauseTimer()
     {
         IsTimerPaused = false;
+    }
+
+    public void Explode()
+    {
+        GameObject.FindGameObjectWithTag(TagComparer.GNOME).GetComponent<Inventory>().RemoveItem(this.gameObject);
+
+        GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<Pickupable>().enabled = true;
+        GetComponent<BoxCollider2D>().enabled = true;
+
+        Timer = 0;
+        PauseTimer();
     }
 
     void Update()
@@ -33,17 +40,5 @@ public class Dynamite : MonoBehaviour
                 Explode();
             }
         }
-    }
-
-    private void Explode()
-    {
-        GameObject.FindGameObjectWithTag(TagComparer.GNOME).GetComponent<Inventory>().RemoveItem(this.gameObject);
-
-        GetComponent<SpriteRenderer>().enabled = true;
-        GetComponent<Pickupable>().enabled = true;
-        GetComponent<BoxCollider2D>().enabled = true;
-
-        Timer = 0;
-        PauseTimer();
     }
 }
