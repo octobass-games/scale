a5e5b04dbf1744454bee0aaa4fb57834d8711d19
Halden <halden@theparkes.me.uk>
2023-11-28 12:48:56 +0000

    fmod event emitter for bomb ticking

--- Assets/Scripts/Dynamite.cs
+++ Assets/Scripts/Dynamite.cs
@@ -1,20 +1,15 @@
+using FMODUnity;
 using UnityEngine;
 
 public class Dynamite : MonoBehaviour
 {
     public float SecondsUntilExplosion;
     public string explosionFmodEvent;
-    public string timerFmodEvent;
+    public StudioEventEmitter SoundEmitter;
 
     private float Timer;
     private bool IsTimerPaused = true;
-    private FMOD.Studio.EventInstance instance;
     private bool isTimerSoundPlaying = true;
-
-    private void Start()
-    {
-        instance = FMODUnity.RuntimeManager.CreateInstance(timerFmodEvent);
-    }
 
     public void PauseTimer()
     {
@@ -30,7 +25,7 @@ public class Dynamite : MonoBehaviour
     public void Explode()
     {
         GameObject.FindGameObjectWithTag(TagComparer.GNOME).GetComponent<Inventory>().RemoveItem(this.gameObject);
-        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        SoundEmitter.Stop();
         FMODUnity.RuntimeManager.PlayOneShot(explosionFmodEvent);
 
         GetComponent<SpriteRenderer>().enabled = true;
@@ -49,14 +44,14 @@ public class Dynamite : MonoBehaviour
 
             if (isTimerSoundPlaying == false)
             {
-                instance.start();
+                SoundEmitter.Play();
                 isTimerSoundPlaying = true;
             }
             
 
             if (Timer > SecondsUntilExplosion)
             {
-                instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                SoundEmitter.Stop();
                 Explode();
             }
         }
