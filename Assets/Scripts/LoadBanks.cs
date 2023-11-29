fb71ac9e586936576f54021e2b5b15916b4ede43
adamskipadamski <adam.t.wright@outlook.com>
2023-11-29 17:04:25 +0000

    add Loading scene to load sound banks

---
+++ Assets/Scripts/LoadBanks.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BankLoader : MonoBehaviour
+{
+    [FMODUnity.BankRef]
+    public List<string> banks;
+
+    public void Start()
+    {
+        foreach (string b in banks)
+        {
+            FMODUnity.RuntimeManager.LoadBank(b, true);
+        }
+
+        /*
+            For Chrome / Safari browsers / WebGL.  Reset audio on response to user interaction (LoadBanks is called from a button press), to allow audio to be heard.
+        */
+        FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+        FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+
+        StartCoroutine(CheckBanksLoadedAndLoadMainMenu());
+    }
+
+    IEnumerator CheckBanksLoadedAndLoadMainMenu()
+    {
+        while (!FMODUnity.RuntimeManager.HaveAllBanksLoaded)
+        {
+            yield return null;
+        }
+
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
