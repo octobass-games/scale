358b643c3f9101620f5f5cc20ff1fad4163f587c
adamskipadamski <adam.t.wright@outlook.com>
2023-12-01 11:35:27 +0000

    add GnomeDoorExit to Tunnel

--- Assets/Scripts/Interactables/Tunnel.cs
+++ Assets/Scripts/Interactables/Tunnel.cs
@@ -3,9 +3,25 @@ using UnityEngine;
 public class Tunnel : MonoBehaviour
 {
     public Transform Exit;
+    public bool IsDoor;
+    public Transform GnomeDoorExit;
 
     public void Navigate(GameObject gameObject)
     {
-        gameObject.GetComponent<CharacterController2D>().ForcePosition(Exit.position);
+        if (IsDoor)
+        {
+            if (TagComparer.IsGnome(gameObject.tag) && GnomeDoorExit != null)
+            {
+                gameObject.GetComponent<CharacterController2D>().ForcePosition(GnomeDoorExit.position);
+            }
+            else
+            {
+                gameObject.GetComponent<CharacterController2D>().ForcePosition(Exit.position);
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<CharacterController2D>().ForcePosition(Exit.position);
+        }
     }
 }
