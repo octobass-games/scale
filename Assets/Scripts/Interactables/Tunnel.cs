8bd2aff816c9dda73a5d92e07af897e4dc9f4ee9
adamskipadamski <adam.t.wright@outlook.com>
2023-12-01 11:46:15 +0000

    improve doors

--- Assets/Scripts/Interactables/Tunnel.cs
+++ Assets/Scripts/Interactables/Tunnel.cs
@@ -4,24 +4,41 @@ public class Tunnel : MonoBehaviour
 {
     public Transform Exit;
     public bool IsDoor;
+    public Transform GiantDoorExit;
     public Transform GnomeDoorExit;
 
     public void Navigate(GameObject gameObject)
     {
+        var characterController = gameObject.GetComponent<CharacterController2D>();
+
         if (IsDoor)
         {
-            if (TagComparer.IsGnome(gameObject.tag) && GnomeDoorExit != null)
+            if (TagComparer.IsGnome(gameObject.tag))
             {
-                gameObject.GetComponent<CharacterController2D>().ForcePosition(GnomeDoorExit.position);
+                if (GnomeDoorExit != null)
+                {
+                    characterController.ForcePosition(GnomeDoorExit.position);
+                }
+                else
+                {
+                    characterController.ForcePosition(Exit.position);
+                }
             }
             else
             {
-                gameObject.GetComponent<CharacterController2D>().ForcePosition(Exit.position);
+                if (GiantDoorExit != null)
+                {
+                    characterController.ForcePosition(GiantDoorExit.position);
+                }
+                else
+                {
+                    characterController.ForcePosition(Exit.position);
+                }
             }
         }
         else
         {
-            gameObject.GetComponent<CharacterController2D>().ForcePosition(Exit.position);
+            characterController.ForcePosition(Exit.position);
         }
     }
 }
